e1579eb3e0a9d2837bb3221bfd54298d6efd7d5f
billhu1996 <billhu1996@gmail.com>
2015-10-20 13:56:41 +0800

    Initial Commit

---
+++ EightQueens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EightQueens
+//
+//  Created by Bill Hu on 15/10/20.
+//  Copyright © 2015年 Bill Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
